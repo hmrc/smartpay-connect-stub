436e642e81f62e4064b07ed5d82cbf2ee27712c8
Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>
2023-02-27 07:53:53 +0000

    [PP][OPS-9685] dependencies and scala upgrades
    Signed-off-by: Pawel Panasewicz <2970040+pawelpanasewicz@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,18 +6,16 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-28"   % "5.18.0",
-    "uk.gov.hmrc"           %% "play-frontend-hmrc"           % "6.2.0-play-28",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-28"           % "0.73.0",
-    "org.julienrf"          %% "play-json-derived-codecs"     % "6.0.0",
+    "uk.gov.hmrc"           %% "bootstrap-frontend-play-28"   % "7.13.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc"           % "6.6.0-play-28",
+    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-28"           % "0.74.0",
+    "org.julienrf"          %% "play-json-derived-codecs"     % "7.0.0",
     "com.beachape"          %% "enumeratum-play"              % "1.7.2",
-    "org.julienrf"          %% "play-json-derived-codecs"     % "6.0.0", //choose carefully
     "org.typelevel"         %% "cats-core"                    % "2.9.0",
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.18.0" % Test,
-    "org.scalatest"           %% "scalatest"                  % "3.2.7"  % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8" % Test,
+    "org.scalatest"           %% "scalatest"                  % "3.2.15"  % Test,
+    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2" % Test,
   )
 }
