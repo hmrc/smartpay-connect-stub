46ce62931392cb8540df787896f34e5cbd58e02e
Jake Barnes <33868990+barnesjake@users.noreply.github.com>
2024-02-20 16:08:13 +0000

    [JB][OPS-11448] remove mongo from repo, it isn't used and is breaking jenkins builds

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt._
 
 object AppDependencies {
 
+  val bootstrapVersion = "8.4.0"
+
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"   % "8.4.0",
+    "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "8.5.0",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"           % "1.7.0",
     "org.julienrf"          %% "play-json-derived-codecs"     % "10.1.0",
     "com.beachape"          %% "enumeratum-play"              % "1.8.0",
     "org.typelevel"         %% "cats-core"                    % "2.10.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalatest"           %% "scalatest"                  % "3.2.17",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.62.2"
+    "uk.gov.hmrc"          %% "bootstrap-test-play-30" % bootstrapVersion
   ).map(_ % Test)
 }
