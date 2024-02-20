1a740d57edc94645a0aabfd098ff0d2c56250b05
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2024-02-20 15:17:01 +0000

    [JB][OPS-11448] remove mongo from repo, it isn't used and is breaking jenkins builds

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,6 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"   % "8.4.0",
     "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "8.5.0",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"           % "1.7.0",
     "org.julienrf"          %% "play-json-derived-codecs"     % "10.1.0",
     "com.beachape"          %% "enumeratum-play"              % "1.8.0",
     "org.typelevel"         %% "cats-core"                    % "2.10.0"
