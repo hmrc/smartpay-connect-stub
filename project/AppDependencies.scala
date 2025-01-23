bbf80c5fa33a95ef1935227ba206effc47fd6a0b
Myles Offord <2564924+Lyrain@users.noreply.github.com>
2025-01-23 10:04:54 +0000

    Fix CSS on scenario page (#60)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "11.10.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "11.11.0",
     "org.julienrf"          %% "play-json-derived-codecs"     % "10.1.0",
     "com.beachape"          %% "enumeratum-play"              % "1.8.0",
     "org.typelevel"         %% "cats-core"                    % "2.10.0"
