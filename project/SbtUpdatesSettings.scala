7e86add806ae863b75ff7637d0808e6e6924e4c4
DarrenCB <58850786+DarrenCB@users.noreply.github.com>
2025-03-06 11:48:42 +0000

    [DCB][OPS-13223] Java 21 update & add dependency update check

---
+++ project/SbtUpdatesSettings.scala
@@ -0,0 +1,14 @@
+import com.timushev.sbt.updates.UpdatesKeys.dependencyUpdates
+import com.timushev.sbt.updates.UpdatesPlugin.autoImport.{dependencyUpdatesFailBuild, dependencyUpdatesFilter, moduleFilterRemoveValue}
+import sbt.Keys.*
+import sbt.*
+
+object SbtUpdatesSettings {
+
+  lazy val sbtUpdatesSettings = Seq(
+    dependencyUpdatesFailBuild := StrictBuilding.strictBuilding.value,
+    (Compile / compile) := ((Compile / compile) dependsOn dependencyUpdates).value,
+    dependencyUpdatesFilter -= moduleFilter("org.scala-lang")
+  )
+
+}
