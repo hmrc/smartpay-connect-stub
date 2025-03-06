bde8d444fdae79255f576ffe53224ff528fd753a
Darren Carver-Balsiger <58850786+DarrenCB@users.noreply.github.com>
2025-03-06 16:41:28 +0000

    [DCB][OPS-13223] Java 21 update & add dependency update check (#61)
    * [DCB][OPS-13223] Java 21 update & add dependency update check
    
    * [DCB] pr-bot commments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,14 +2,14 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.6.0"
+  val bootstrapVersion = "9.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "11.11.0",
-    "org.julienrf"          %% "play-json-derived-codecs"     % "10.1.0",
-    "com.beachape"          %% "enumeratum-play"              % "1.8.0",
-    "org.typelevel"         %% "cats-core"                    % "2.10.0"
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "11.12.0",
+    "org.julienrf"          %% "play-json-derived-codecs"     % "11.0.0",
+    "com.beachape"          %% "enumeratum-play"              % "1.8.2",
+    "org.typelevel"         %% "cats-core"                    % "2.13.0"
   )
 
   val test: Seq[ModuleID] = Seq(
