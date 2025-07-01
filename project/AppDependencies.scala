93143f336fe7add02769752e76ab3b8692e0e7b9
Andrew Chung <26929776+AndyHWChung@users.noreply.github.com>
2025-07-01 09:09:23 +0100

    [AC][OPS-13710] add digital-service label (#65)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "12.5.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "12.7.0",
     "org.julienrf"          %% "play-json-derived-codecs"     % "11.0.0",
     "com.beachape"          %% "enumeratum-play"              % "1.9.0",
     "org.typelevel"         %% "cats-core"                    % "2.13.0"
