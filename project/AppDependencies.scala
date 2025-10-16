0bc4e2b1bf7630af4b701603273fbf3a5445b024
Darren Carver-Balsiger <58850786+DarrenCB@users.noreply.github.com>
2025-10-16 09:50:21 +0100

    [DCB][OPS-14201] dependency update (#66)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "10.3.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"   % bootstrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "12.7.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"   % "12.17.0",
     "org.julienrf"          %% "play-json-derived-codecs"     % "11.0.0",
     "com.beachape"          %% "enumeratum-play"              % "1.9.0",
     "org.typelevel"         %% "cats-core"                    % "2.13.0"
